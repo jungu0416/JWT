1e5abc7b832abcff68d2fb5a0da256d945000ce4
jungu0416 <junguYY0809@gmail.com>
2022-04-20 15:31:50 +0900

    gradle

--- src/main/java/dev/potatoo/projectlist/ProjectlistApplication.java
+++ src/main/java/dev/potatoo/projectlist/ProjectlistApplication.java
@@ -2,10 +2,17 @@ package dev.potatoo.projectlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectlistApplication {
+public class ProjectlistApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjectlistApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectlistApplication.class, args);
 	}
